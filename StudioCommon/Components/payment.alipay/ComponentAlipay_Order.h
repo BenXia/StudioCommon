b15bcae2541770107e60a878cd7c5c8e2aa7567f
wangdachui <1179763289@qq.com>
2016-02-27 11:14:09 +0800

    修改alipayOrder的参数

--- StudioCommon/Components/payment.alipay/ComponentAlipay_Order.h
+++ StudioCommon/Components/payment.alipay/ComponentAlipay_Order.h
@@ -10,7 +10,7 @@
 
 @interface ComponentAlipay_Order : NSObject
 
-@property (nonatomic, strong) NSString *no;         // 订单ID（由商家自行制定）
+@property (nonatomic, strong) NSString *ID;         // 订单ID（由商家自行制定）
 @property (nonatomic, strong) NSString *name;       // 商品标题
 @property (nonatomic, strong) NSString *desc;       // 商品描述
 @property (nonatomic, strong) NSString *price;      // 商品价格
